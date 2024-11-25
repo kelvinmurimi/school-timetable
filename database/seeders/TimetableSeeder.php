8c6bfddea37d5615db4be31f1c9c9967ea870707
kelvinmurimi <murimikelvin229@gmail.com>
2024-11-25 08:04:05 +0300

    bug fix in timetable Seeeder

--- database/seeders/TimetableSeeder.php
+++ database/seeders/TimetableSeeder.php
@@ -18,103 +18,92 @@ class TimetableSeeder extends Seeder
     {
         //
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Monday',
             'time'=>'8:00Am-10:00Am',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Wednesday',
             'time'=>'10:00Am-12:00Am',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Tuesday',
             'time'=>'8:00Am-10:00Am',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Monday',
             'time'=>'12:00Am-1:00Pm',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Friday',
             'time'=>'8:00Am-10:00Am',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Thursday',
             'time'=>'8:00Am-10:00Am',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Tuesday',
             'time'=>'8:00Am-10:00Am',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Monday',
             'time'=>'3:00Pm-4:00Pm',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Wednesday',
             'time'=>'10:00Am-11:00Am',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Teusday',
             'time'=>'12:00Am-2:00Pm',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
         Timetable::create([
-            'subject_id'=>Subject::all()->random(),
-            'teacher_id'=>Teacher::all()->random(),
+            'subject_id'=>Subject::all()->random()->id,
+            'teacher_id'=>Teacher::all()->random()->id,
             'day'=>'Monday',
             'time'=>'10:00Am-12:00Am',
-            'room_id'=>Room::all()->random(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'room_id'=>Room::all()->random()->id,
+
         ]);
     }
 }
