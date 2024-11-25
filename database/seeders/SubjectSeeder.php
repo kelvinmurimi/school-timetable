085bfe0a39047f341ef2b31a53f8319cc643711e
kelvinmurimi <murimikelvin229@gmail.com>
2024-11-25 06:47:48 +0300

    Add Seeder to Subjects

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Subject;
 class SubjectSeeder extends Seeder
 {
     /**
@@ -13,5 +13,47 @@ class SubjectSeeder extends Seeder
     public function run(): void
     {
         //
+        Subject::create([
+            'subject_name'=>'English',
+            'subject_code'=>'101',
+            'subject_teacher'=>'teacher 1',
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        Subject::create([
+            'subject_name'=>'Maths',
+            'subject_code'=>'102',
+            'subject_teacher'=>'teacher 2',
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        Subject::create([
+            'subject_name'=>'Kiswahili',
+            'subject_code'=>'103',
+            'subject_teacher'=>'teacher 3',
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        Subject::create([
+            'subject_name'=>'Science',
+            'subject_code'=>'104',
+            'subject_teacher'=>'teacher 4',
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        Subject::create([
+            'subject_name'=>'CRE',
+            'subject_code'=>'105',
+            'subject_teacher'=>'teacher 6',
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        Subject::create([
+            'subject_name'=>'Social Studies',
+            'subject_code'=>'106',
+            'subject_teacher'=>'teacher 5',
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
     }
 }
